fed61e7409b33f4d85da14456ceb811f64865f2d
GustavoGNicolaci <gustavo_nicolaci@hotmail.com>
2026-01-20 22:41:58 -0300

    feat: Novo Controller criado por artisan

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
-Route::get('/', [ProdutoController::class, 'index'])->name('produtos.index');
-
-Route::get('/produtos/{id?}', [ProdutoController::class, 'show'])->name('produtos.show');
+Route::resource('produtos', ProdutoController::class);
