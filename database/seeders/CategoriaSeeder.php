a888fe8ddaf4fcf84c5ca2f9577c9c831279180e
GustavoGNicolaci <gustavo_nicolaci@hotmail.com>
2026-01-21 18:37:21 -0300

    feat: seed de categorias e testando JSON

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Categoria;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Categoria::create([
+            'nome' => 'Eletrônicos',
+        ]);
+        Categoria::create([
+            'nome' => 'Roupas',
+        ]);
+        Categoria::create([
+            'nome' => 'Alimentos',
+        ]);
+        Categoria::create([
+            'nome' => 'Livros',
+        ]);
+        Categoria::create([
+            'nome' => 'Móveis',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\CategoriaController;
 
 Route::resource('produtos', ProdutoController::class);
+Route::resource('categorias', CategoriaController::class);
+
