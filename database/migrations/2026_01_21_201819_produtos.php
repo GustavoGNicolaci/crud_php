46b8b0c3f963e5db1e7b2d6294f277e4023d0280
GustavoGNicolaci <gustavo_nicolaci@hotmail.com>
2026-01-21 17:39:39 -0300

    feat: Tabela produto, suas colunas e Model

--- database/migrations/2026_01_21_201819_produtos.php
+++ database/migrations/2026_01_21_201819_produtos.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome');
+            $table->text('descricao')->nullable();
+            $table->decimal('preco', 8, 2);
+            $table->integer('quantidade');
         });
     }
 
