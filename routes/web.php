5ce125cff2b322c1e43ebde8da6109b6cbfc2ced
GustavoGNicolaci <gustavo_nicolaci@hotmail.com>
2026-01-20 22:28:13 -0300

    feat: Configuracao do postgree e teste de Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProdutoController::class, 'index'])->name('produtos.index');
+
+Route::get('/produtos/{id?}', [ProdutoController::class, 'show'])->name('produtos.show');
